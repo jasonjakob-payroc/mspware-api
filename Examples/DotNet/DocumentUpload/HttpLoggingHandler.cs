547926b45e709dcb4cfca5518028053829f3c3c6
jjakob-payroc <112723629+jjakob-payroc@users.noreply.github.com>
2023-07-27 22:00:13 -0400

    Initial commit

---
+++ Examples/DotNet/DocumentUpload/HttpLoggingHandler.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * A C# Example on how to upload a supporting document using the fidano/payroc/pulsepoint api
+ * Using this source code you accept there are no guarantees
+ * jason.jakob@payroc.com
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroc.MSPWareApi.Examples.DotNet.DocumentUpload
+{
+    public class HttpLoggingHandler : DelegatingHandler
+    {
+        public HttpLoggingHandler(HttpMessageHandler innerHandler)
+            : base(innerHandler)
+        {
+        }
+
+        /// <summary>
+        /// Provides a mechanism to log the raw HTTP request send and response
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Request:");
+            Console.WriteLine(request.ToString());
+            //if (request.Content != null)
+            //{
+            //    Console.WriteLine(await request.Content.ReadAsStringAsync());
+            //}
+            //Console.WriteLine();
+
+            HttpResponseMessage response = await base.SendAsync(request, cancellationToken);
+
+            Console.WriteLine("Response:");
+            Console.WriteLine(response.ToString());
+            if (response.Content != null)
+            {
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+            Console.WriteLine();
+
+            return response;
+        }
+    }
+}
---
+++ Examples/DotNet/DocumentUpload/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * A C# Example on how to upload a supporting document using the fidano/payroc/pulsepoint api
+ * Using this source code you accept there are no guarantees
+ * jason.jakob@payroc.com
+ */
+using Payroc.MSPWareApi.Examples.DotNet.DocumentUpload;
+using System.Net.Http.Headers;
+
+string apiurl = "http://localapi.mspware.com:3000";
+string appid = "your app id";
+string appkey = "your app key";
+string merchantApplicationNo = "95649";
+
+//load the file and stuff it into multipart form data
+string filePath = Environment.CurrentDirectory + @"\\dummy.pdf";
+using var form = new MultipartFormDataContent();
+using var fileContent = new ByteArrayContent(await File.ReadAllBytesAsync(filePath));
+fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse("multipart/form-data");
+form.Add(fileContent, "file", Path.GetFileName(filePath)); //this important
+
+//send
+var httpClient = new HttpClient(new HttpLoggingHandler(new HttpClientHandler()))
+{
+    BaseAddress = new Uri($"{apiurl}/{appid}/v2/")
+};
+httpClient.DefaultRequestHeaders.Add("X-Api-Key", appkey);
+var response = await httpClient.PostAsync($"applications/{merchantApplicationNo}/documents", form);
+var responseContent = await response.Content.ReadAsStringAsync();
+Console.WriteLine("response :" + responseContent);
